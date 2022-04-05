1718b50b18233e035838cdbdf8e6d72f10df35db
aula <martinconcetti97@gmail.com>
2022-04-05 11:32:29 -0300

    Two planes, two cameras, two players, invisible walls

---
+++ Two-player/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed = 6;
+    [SerializeField] private float jumpSpeed;
+
+     void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(vertical, 0f, -horizontal).normalized;
+
+        if (direction.magnitude >= 0.1f) 
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+    }
+}
