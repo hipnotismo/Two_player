cef4aa72d73b7be2e5bc0dad323a178848270ce1
aula <martinconcetti97@gmail.com>
2022-04-05 12:05:57 -0300

    Basic jump

--- Two-player/Assets/movement.cs
+++ Two-player/Assets/movement.cs
@@ -6,14 +6,26 @@ public class movement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private float speed = 6;
-    [SerializeField] private float jumpSpeed;
-
-     void Update()
+    [SerializeField] private float jumpSpeed = 3.5f;
+    [SerializeField] private float gravity = 9.5f;
+    private float directionY;
+    void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(vertical, 0f, -horizontal).normalized;
 
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                directionY = jumpSpeed;
+            }
+        }
+      
+        directionY -= gravity * Time.deltaTime;
+        direction.y = directionY;
         if (direction.magnitude >= 0.1f) 
         {
             controller.Move(direction * speed * Time.deltaTime);
